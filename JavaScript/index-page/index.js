38f7136faf416723084b04e559872988f8443b97
Chef91 <chefgraham1991@gmail.com>
2025-10-21 14:41:15 -0400

    small update

--- JavaScript/index-page/index.js
+++ JavaScript/index-page/index.js
@@ -1,5 +1,20 @@
-document.getElementsByid("learn-more").addEventListener("click", () => {
-  window.location.href = "hof.html";
+
+
+document.getElementByid("learn-more").addEventListener("click", () => {
+  window.location.href = "./hof.html";
 })
 
 // Not working - learn more = goes to hof game page
+
+//game-card
+function updateCard(imageSrc, text) {
+  document.getElementById('game-header').innerHTML = profile.header;
+  document.getElementById('game-text').innerHTML = profile.text;
+}
+//cards
+const cards = {
+  cardDND: {
+    header: 'Dungeons & Dragons',
+    
+  }
+}
