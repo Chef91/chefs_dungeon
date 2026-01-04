4626c151dc021a7c6bdb4d69e24e4104048db420
Chef91 <chefgraham1991@gmail.com>
2026-01-03 20:58:22 -0500

    Refactors index page and styles footer
    Moves campaign page to dedicated directory.
    Links on index page now point to the new directory.
    
    Adds a border to the top of sections and the footer to improve UI.
    Improves styling to the footer.
    Restyles game master section to improve UI.

--- JavaScript/index-page/index.js
+++ JavaScript/index-page/index.js
@@ -1,4 +1,17 @@
 // Learn More = Links to hof.html
 document.getElementById("learn-more").addEventListener("click", () => {
-  window.location.href = "./hof.html";
+  window.location.href = "./pages/campaigns/hof.html";
+});
+// Nav Links 
+document.getElementById("nav-campaign").addEventListener("click", () => {
+  window.location.href = "./pages/campaigns/campaign.html";
+});
+document.getElementById("nav-news").addEventListener("click", () => {
+  window.location.href = "./pages/campaigns/campaign.html";
+});
+document.getElementById("nav-games").addEventListener("click", () => {
+  window.location.href = "./pages/campaigns/campaign.html";
+});
+document.getElementById("nav-contact").addEventListener("click", () => {
+  window.location.href = "./pages/campaigns/campaign.html";
 });
